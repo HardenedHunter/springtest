1c628e63c34fe16ee6ed9d9b79da0b65f8a98a70
Artyom Khudyakov <specialforbattlenet@gmail.com>
2020-10-05 18:39:38 +0300

    Application setup
    Added basic controller and maven settings

---
+++ src/main/java/com/example/springtest/controller/api/AboutController.java
@@ -0,0 +1,28 @@
+package com.example.springtest.controller.api;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.Api;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/about")
+public class AboutController {
+    @Value("${application.name}")
+    private String applicationName;
+
+    @Value("${build.version}")
+    private String buildVersion;
+
+    @GetMapping
+    public Map<String, String> getInfo() {
+        var result = new HashMap<String, String>();
+        result.put("name", applicationName);
+        result.put("version", buildVersion);
+        return result;
+    }
+}
